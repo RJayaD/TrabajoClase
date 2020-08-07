bd1dcc6d2f32e8b0f162c4591272fe6883a50891
RubenJayaP <66756477+RJayaD@users.noreply.github.com>
2020-08-06 19:00:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/trabajoclase/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.trabajoclase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<String> permisos = new ArrayList<String>();
+        permisos.add(Manifest.permission.CAMERA);
+        permisos.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        permisos.add(Manifest.permission.WRITE_CALENDAR);
+        getPermission(permisos);
+        imageView = (ImageView) findViewById(R.id.foto);
+    }
+
+    public void getPermission(ArrayList<String> permisosSolicitados){
+
+        ArrayList<String> listPermisosNOAprob = getPermisosNoAprobados(permisosSolicitados);
+        if (listPermisosNOAprob.size()>0)
+            if (Build.VERSION.SDK_INT >= 23)
+                requestPermissions(listPermisosNOAprob.toArray(new String[listPermisosNOAprob.size()]), 1);
+
+    }
+
+
+    public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
+        ArrayList<String> list = new ArrayList<String>();
+        for(String permiso: listaPermisos) {
+            if (Build.VERSION.SDK_INT >= 23)
+                if(checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED)
+                    list.add(permiso);
+
+        }
+        return list;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        String s="";
+        if(requestCode==1)    {
+            for(int i =0; i<permissions.length;i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    s=s + "OK " + permissions[i] + "\n";
+                else
+                    s=s + "NO  " + permissions[i] + "\n";
+            }
+            Toast.makeText(this.getApplicationContext(), s, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void MostrarDescargas(View view){
+
+        Intent intent = new Intent();
+        intent.setAction(DownloadManager.ACTION_VIEW_DOWNLOADS);
+        startActivity(intent);
+
+    }
+    public void BajarDoc(View view){
+        String url = "https://www.uteq.edu.ec/revistacyt/archivositio/instrucciones_arbitros.pdf";
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("PDF");
+        request.setTitle("Pdf");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            request.allowScanningByMediaScanner();
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        }
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "filedownload.pdf");
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        try {
+            manager.enqueue(request);
+        } catch (Exception e) {
+            Toast.makeText(this.getApplicationContext(),"Error: "  + e.getMessage(),Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void TakingPhoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    public void MostrarPhoto(View view){
+        TakingPhoto();
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
+
+}
